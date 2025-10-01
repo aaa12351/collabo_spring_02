980981f3bf548fd434a1840a1313440c04c2604b
lee <lee@example.com>
2025-10-01 17:06:26 +0900

    spring 2025/10/01

---
+++ src/main/java/com/coffee/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.coffee.service;
+
+import com.coffee.entity.Order;
+import com.coffee.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository ;
+
+
+    public void saveOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public List<Order> findByMemberId(Long memberId) {
+        return orderRepository.findByMemberIdOrderByIdDesc(memberId);
+    }
+
+    public List<Order> findAllOrders() {
+        return orderRepository.findAllByOrderByIdDesc() ;
+    }
+}
